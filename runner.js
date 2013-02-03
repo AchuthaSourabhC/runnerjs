756ce15364becc791ed86a01bd34d54ec8e47871
SourabhCA <sourabh2393@hotmail.com>
2013-02-03 14:57:34 +0530

    initialize

--- runner.js
+++ runner.js
@@ -2,8 +2,8 @@ var childProcess = require('child_process')
 	, async = require('async')
 	, ex1,ex2;
 
-exprots.run = function(){
-    ex1 = childProcess.exec('pwd',
+exports.run = function(){
+    ex1 = childProcess.exec('g++ -o test test.cpp',
         function (error, stdout, stderr) {
             if (error) {
               console.log(error.stack);
@@ -12,6 +12,19 @@ exprots.run = function(){
             }
             console.log('Child Process STDOUT: '+stdout);
             console.log('Child Process STDERR: '+stderr);
+            ex2 = childProcess.exec('test.exe < input.txt > output.txt',
+                function (error, stdout, stderr) {
+                    if (error) {
+                      console.log(error.stack);
+                      console.log('Error code: '+error.code);
+                      console.log('Signal received: '+error.signal);
+                    }
+                    console.log('Child Process STDOUT: '+stdout);
+                    console.log('Child Process STDERR: '+stderr);
+                    });
+            ex2.on('exit',function(error){
+                console.log('Process exited with error status: '+error);
+            });
         });
     ex1.on('exit',function(error){
         console.log('Process exited with error status: '+error);
